84f2f27bd75793b0170fa0be31c3646f8441a05a
afomike <temidayoafote@gmail.com>
2025-09-11 13:46:23 +0100

    done

--- Version_check.py
+++ Version_check.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 import sklearn
 import matplotlib
+import sys
+
 
 print("Package Versions:")
 print(f"Flask:         {flask.__version__}")
@@ -10,4 +12,5 @@ print(f"Numpy:         {np.__version__}")
 print(f"Pandas:        {pd.__version__}")
 print(f"Scikit-learn:  {sklearn.__version__}")
 print(f"Matplotlib:    {matplotlib.__version__}")
+print(f"python:    {sys.version}")
 # print(f"SHAP:          {shap.__version__}")
